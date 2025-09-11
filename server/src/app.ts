fcead166e27af7696be4b1feda45df9b79527e3c
mohdsaadshaikh <mohammadsaad925s4s@gmail.com>
2025-09-11 12:33:41 +0500

    Add axios configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,8 +8,8 @@ const app = new Hono();
 
 app.use("*", sessionMiddleware);
 
-app.route("/auth", authRoutes);
-app.route("/users", userRoutes);
+app.route("/api/v1/auth", authRoutes);
+app.route("/api/v1/users", userRoutes);
 
 app.get("/", requireRole(Role.LIBRARIAN), (c) => {
   return c.text("Hello bhai Hono!");
